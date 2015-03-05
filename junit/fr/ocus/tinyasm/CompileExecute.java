56ba524133a02104e340f8bec80f3d6b46bb764b
OcuS <matthieu.honel@free.fr>
2015-03-06 00:08:27 +0100

    All tests done

--- junit/fr/ocus/tinyasm/CompileExecute.java
+++ junit/fr/ocus/tinyasm/CompileExecute.java
@@ -34,329 +34,440 @@ public class CompileExecute {
 
     @Test
     public void and_0x00() {
-        compileExecute("and_0x00", new Screen() {
+        compileExecute("and_0x00", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(10 & 3, value);
             }
-        });
+        }));
     }
 
     @Test
     public void and_0x01() {
-        compileExecute("and_0x01", new Screen() {
+        compileExecute("and_0x01", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(6 & 13, value);
             }
-        });
+        }));
     }
 
     @Test
     public void or_0x02() {
-        compileExecute("or_0x02", new Screen() {
+        compileExecute("or_0x02", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(10 | 3, value);
             }
-        });
+        }));
     }
 
     @Test
     public void or_0x03() {
-        compileExecute("or_0x03", new Screen() {
+        compileExecute("or_0x03", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(6 | 13, value);
             }
-        });
+        }));
     }
 
     @Test
     public void xor_0x04() {
-        compileExecute("xor_0x04", new Screen() {
+        compileExecute("xor_0x04", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(6 ^ 23, value);
             }
-        });
+        }));
     }
 
     @Test
     public void xor_0x05() {
-        compileExecute("xor_0x05", new Screen() {
+        compileExecute("xor_0x05", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(7 ^ 15, value);
             }
-        });
+        }));
     }
 
     @Test
     public void not_0x06() {
-        compileExecute("not_0x06", new Screen() {
+        compileExecute("not_0x06", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
-                Assert.assertEquals(~8, value);
+                Assert.assertEquals(~7, value);
             }
-        });
+        }));
     }
 
     @Test
     public void mov_0x07() {
-        compileExecute("mov_0x07", new Screen() {
-            @Override
-            public void printDecimal(final int value) {
-                Assert.assertEquals(15, value);
-            }
-        });
-    }
-
-    @Test
-    public void mov_0x08() {
-        compileExecute("mov_0x08", new Screen() {
+        compileExecute("mov_0x07", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(29, value);
             }
-        });
+        }));
+    }
+
+    @Test
+    public void mov_0x08() {
+        compileExecute("mov_0x08", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(15, value);
+            }
+        }));
     }
 
     @Test
     public void random_0x09() {
-        compileExecute("random_0x09", new Screen() {
-            @Override
-            public void printDecimal(final int value) {
-                Assert.assertTrue(value >= 0 && value <= 25);
-            }
-        });
-        compileExecute("random_0x09", new Screen() {
-            @Override
-            public void printDecimal(final int value) {
-                Assert.assertTrue(value >= 0 && value <= 25);
-            }
-        });
-        compileExecute("random_0x09", new Screen() {
-            @Override
-            public void printDecimal(final int value) {
-                Assert.assertTrue(value >= 0 && value <= 25);
-            }
-        });
+        for (int i = 0; i <= 25; i++) {
+            compileExecute("random_0x09", new ScreenWrapper(new Screen() {
+                @Override
+                public void printDecimal(final int value) {
+                    Assert.assertTrue(value >= 0 && value <= 25);
+                }
+            }));
+        }
     }
 
     @Test
     public void add_0x0A() {
-        compileExecute("add_0x0A", new Screen() {
+        compileExecute("add_0x0A", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(10 + 9, value);
             }
-        });
+        }));
     }
 
     @Test
     public void add_0x0B() {
-        compileExecute("add_0x0B", new Screen() {
+        compileExecute("add_0x0B", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(23 + 14, value);
             }
-        });
+        }));
     }
 
     @Test
     public void sub_0x0C() {
-        compileExecute("sub_0x0C", new Screen() {
+        compileExecute("sub_0x0C", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(16 - 7, value);
             }
-        });
+        }));
     }
 
     @Test
     public void sub_0x0D() {
-        compileExecute("sub_0x0D", new Screen() {
+        compileExecute("sub_0x0D", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(9 - 7, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jmp_0x0E() {
-        compileExecute("jmp_0x0E", new Screen() {
+        compileExecute("jmp_0x0E", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(10, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jmp_0x0F() {
-        compileExecute("jmp_0x0F", new Screen() {
+        compileExecute("jmp_0x0F", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(19, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jz_0x10() {
-        compileExecute("jz_0x10", new Screen() {
+        compileExecute("jz_0x10", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(88, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jz_0x11() {
-        compileExecute("jz_0x11", new Screen() {
+        compileExecute("jz_0x11", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(77, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jz_0x12() {
-        compileExecute("jz_0x12", new Screen() {
+        compileExecute("jz_0x12", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(44, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jz_0x13() {
-        compileExecute("jz_0x13", new Screen() {
+        compileExecute("jz_0x13", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(22, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jeq_0x14() {
-        compileExecute("jeq_0x14", new Screen() {
+        compileExecute("jeq_0x14", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
                 Assert.assertEquals(200, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jeq_0x15() {
-        compileExecute("jeq_0x15", new Screen() {
+        compileExecute("jeq_0x15", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
-                Assert.assertEquals(10 + 9, value);
+                Assert.assertEquals(200, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jeq_0x16() {
-        compileExecute("jeq_0x16", new Screen() {
+        compileExecute("jeq_0x16", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
-                Assert.assertEquals(10 + 9, value);
+                Assert.assertEquals(123, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jeq_0x17() {
-        compileExecute("jeq_0x17", new Screen() {
+        compileExecute("jeq_0x17", new ScreenWrapper(new Screen() {
             @Override
             public void printDecimal(final int value) {
-                Assert.assertEquals(10 + 9, value);
+                Assert.assertEquals(752, value);
             }
-        });
+        }));
     }
 
     @Test
     public void jls_0x18() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jls_0x18", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(454, value);
+            }
+        }));
     }
 
     @Test
     public void jls_0x19() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jls_0x19", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(978, value);
+            }
+        }));
     }
 
     @Test
     public void jls_0x1A() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jls_0x1A", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(699, value);
+            }
+        }));
     }
 
     @Test
     public void jls_0x1B() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jls_0x1B", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(945, value);
+            }
+        }));
     }
 
     @Test
     public void jgt_0x1C() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jgt_0x1C", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(869, value);
+            }
+        }));
     }
 
     @Test
     public void jgt_0x1D() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jgt_0x1D", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(456, value);
+            }
+        }));
     }
 
     @Test
     public void jgt_0x1E() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jgt_0x1E", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(852, value);
+            }
+        }));
     }
 
     @Test
     public void jgt_0x1F() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("jgt_0x1F", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(951, value);
+            }
+        }));
     }
 
     @Test
     public void halt_0xFF() {
-        Assert.fail("Not implemented yet.");
-    }
+        compileExecute("halt_0xFF", new ScreenWrapper(false, new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.fail("Nothing should have been printed!");
+            }
 
-    @Test
-    public void aprint_0x21() {
-        Assert.fail("Not implemented yet.");
+            @Override
+            public void printAscii(final int value) {
+                Assert.fail("Nothing should have been printed!");
+            }
+        }));
     }
 
     @Test
     public void aprint_0x20() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("aprint_0x20", new ScreenWrapper(new Screen() {
+            @Override
+            public void printAscii(final int value) {
+                Assert.assertEquals(896, value);
+            }
+        }));
     }
 
     @Test
-    public void dprint_0x23() {
-        Assert.fail("Not implemented yet.");
+    public void aprint_0x21() {
+        compileExecute("aprint_0x21", new ScreenWrapper(new Screen() {
+            @Override
+            public void printAscii(final int value) {
+                Assert.assertEquals(523, value);
+            }
+        }));
     }
 
     @Test
     public void dprint_0x22() {
-        Assert.fail("Not implemented yet.");
+        compileExecute("dprint_0x22", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(214, value);
+            }
+        }));
     }
 
-    private abstract class Screen implements IScreen {
+    @Test
+    public void dprint_0x23() {
+        compileExecute("dprint_0x23", new ScreenWrapper(new Screen() {
+            @Override
+            public void printDecimal(final int value) {
+                Assert.assertEquals(236, value);
+            }
+        }));
+    }
+
+    static private class Screen implements IScreen {
+
         @Override
-        final public void printAscii(final int value) {
-            // nothing
+        public void printAscii(int value) {
+            Assert.fail("Nothing should have print!");
+        }
+
+        @Override
+        public void printDecimal(int value) {
+            Assert.fail("Nothing should have print!");
         }
     }
 
-    private void compileExecute(final String name, final Screen screen) {
+    static private interface ITestScreen extends IScreen {
+        boolean shouldHavePrint();
+
+        boolean hasPrint();
+    }
+
+    static private final class ScreenWrapper implements ITestScreen {
+        private final Screen screen;
+        private final boolean shouldHavePrint;
+
+        private boolean hasPrint = false;
+
+        public ScreenWrapper(final Screen screen) {
+            this(true, screen);
+        }
+
+        public ScreenWrapper(final boolean shouldHavePrint, final Screen screen) {
+            this.shouldHavePrint = shouldHavePrint;
+            this.screen = screen;
+        }
+
+        @Override
+        public void printAscii(int value) {
+            screen.printAscii(value);
+            hasPrint = true;
+        }
+
+        @Override
+        public void printDecimal(int value) {
+            screen.printDecimal(value);
+            hasPrint = true;
+        }
+
+        @Override
+        public boolean shouldHavePrint() {
+            return shouldHavePrint;
+        }
+
+        @Override
+        public boolean hasPrint() {
+            return hasPrint;
+        }
+    }
+
+
+    private void compileExecute(final String name, final ITestScreen screenWrapper) {
         final String baseDir = System.getProperty("user.dir") + "\\programs\\unit\\";
 
         final String inputFilePath = baseDir + name + ".asm";
@@ -371,8 +482,14 @@ public class CompileExecute {
         }
         final FileOutputStream out = null;
         try {
-            final VM vm = new VM(screen);
+            final VM vm = new VM(screenWrapper);
             vm.run(Compiler.get().compile(asmContent));
+            if (screenWrapper.shouldHavePrint() && !screenWrapper.hasPrint()) {
+                Assert.fail("Nothing was printed!");
+            }
+            if (!screenWrapper.shouldHavePrint() && screenWrapper.hasPrint()) {
+                Assert.fail("Nothing should have been printed!");
+            }
         } catch (final ASMWrongArgumentCountException e) {
             e.printStackTrace();
             Assert.fail(getStackTrace(e));
@@ -401,5 +518,4 @@ public class CompileExecute {
         e.printStackTrace(pw);
         return sw.toString();
     }
-
 }
